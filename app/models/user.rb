0ebcdf200cd91c769338fad7d8bbb14c3ed64102
Naya  Willis <greedybrain18@gmail.com>
2020-07-07 13:08:31 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
+  
 class User < ActiveRecord::Base
-  include Slugifiable::InstanceMethods
-  extend Slugifiable::ClassMethods
+  has_many  :tweets
+
+
   has_secure_password
-  has_many :tweets
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
 end
